6a7e69624b47fbbe652f2afd3e16d28ff1c86f73
rshaaadd <104209079+rshaaadd@users.noreply.github.com>
2023-11-13 18:35:43 +0700

    fix
    fix node

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { useColorScheme } from 'nativewind';
-import { SafeAreaView,StyleSheet, Text, View, Switch} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView,StyleSheet, Text, View, Switch} from 'react-native';
+import { useColorScheme } from 'nativewind';
 import ProductList from './components/ProductsList';
 
 export default function App() {
@@ -9,8 +9,9 @@ export default function App() {
   return (
   <SafeAreaView className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
     <View className={'flex-row w-full gap-5'}>
+    <Text className="dark:text-white text-2xl font-bold">New collection</Text>
       <Switch value={colorScheme==='dark'}onChange={toggleColorScheme}/>
-      <Text className="dark:text-white text-2xl font-bold">New collection</Text>
+      
          </View>
    
     <ProductList/>
